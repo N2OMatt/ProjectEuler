f00a9f698375cc2e4ba0f62c64687b03c50da7dc
N2OMatt <n2o.matt@gmail.com>
2015-09-19 17:17:01 -0300

    Change the range to xrange.

--- problems/euler06/euler06.py
+++ problems/euler06/euler06.py
@@ -9,18 +9,18 @@ import eulersupport;
 #   12 + 22 + ... + 102 = 385
 # The square of the sum of the first ten natural numbers is,
 #   (1 + 2 + ... + 10)2 = 552 = 3025
-# Hence the difference between the sum of the squares of the first ten natural 
+# Hence the difference between the sum of the squares of the first ten natural
 # numbers and the square of the sum is 3025 - 385 = 2640.
-# Find the difference between the sum of the squares of the first one hundred 
+# Find the difference between the sum of the squares of the first one hundred
 # natural numbers and the square of the sum.
 def run(upperbound):
-    numbers = range(1, upperbound + 1); #All numbers up to upperbound. 
-    
+    numbers = xrange(1, upperbound + 1); #All numbers up to upperbound.
+
     sum_of_squares = sum(map(pow, numbers, [2] * (upperbound)));
     square_of_sums = sum(numbers) ** 2;
-    
+
     result = (square_of_sums - sum_of_squares);
-    
+
     #Report completion.
     eulersupport.write_output(result);
 
@@ -28,9 +28,9 @@ def run_full():
     eulersupport.name = "euler06";
     run(100);
 
-def run_test():        
+def run_test():
     eulersupport.name = "euler06-Test";
-    run(10);    
+    run(10);
 
 def main():
     run_mode = eulersupport.get_run_mode();
